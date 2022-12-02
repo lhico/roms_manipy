f9e7763ca534f4b8fd6def21afc974491dc52472
Danilo A. Silva <nilodna@gmail.com>
2022-12-02 12:25:23 -0300

    adding a first version of Transect(), a class to perform an extraction of cross section based on a list of lons,lats, which can be, for instance, CTD profiles locations

--- roms_manipy/visualize.py
+++ roms_manipy/visualize.py
@@ -2,7 +2,15 @@ from typing import Optional as _opt
 import matplotlib.pyplot as plt
 from matplotlib import figure as _figure
 from matplotlib import axes as _axes
+import cartopy.crs as ccrs
 import xarray as _xr
+import numpy as np
+
+
+try:
+    from sectionate.section import create_section, create_section_composite
+except:
+    raise ValueError("Please, install sectionate, otherwise Transect will be not available.")
 
 def fig_ax_ok(fig: _opt[_figure.Figure],
               ax : _opt[_axes._subplots.Axes]):
@@ -188,3 +196,209 @@ def plot(ds1    : _opt[_xr.core.dataset.Dataset],
     pt = dict_plot[ptype](ds1)(ax=ax, **plotkwargs)
 
     return fig, ax, pt
+
+
+class Transect(object):
+    """
+    Example usage
+    --------------    
+    >>> lons = [-46.37016667, -46.13716667, -45.83833333, -45.59666667]
+    >>> lats = [-24.16083333, -24.43333333, -24.781     , -25.06333333]
+    >>> ds = xroms.open_netcdf('roms_avg.nc', chunks={'ocean_time': 50})
+    >>> transect = Transect(ds, xstr='lon_rho', ystr='lat_rho')
+    >>> transect.create_sections(lons, lats, varbs=['temp', 'salt'])
+    >>> transect._plot(varb='temp', x='latitude', y='vertical', cmap=cmo.thermal)
+
+    """
+
+    def __init__(self, data, xstr='lon_rho', ystr='lat_rho'):
+        """
+        TODO: add docstring adequadamente
+        xstr,ystr: lon_rho,lat_rho, but also could be lon_psi,lat_psi depending on the 
+                   variable you are working with
+        """
+        self.data = data
+        # extracting numerical grid
+        self.lon = self.data[xstr]
+        self.lat = self.data[ystr]
+
+    ##################################################################################
+    ##---- functions to create a cross section based on two given coordinates ------##
+    ##################################################################################
+    def create_transect(self, begin=None, final=None, lons=None, lats=None, varbs=None, 
+                              times=None, find=True, forced_limits=False):
+        """
+            todo: documentation
+        """
+        if begin and final:
+            print('Searching the nearest indexes for a straight line between two coordinates ...')
+            # initially, we search for the nearest grid cells to a straight line between
+            # the initial and final coordinates provided
+            lon0, lat0 = begin
+            lon1, lat1 = final
+
+            self.lons = [lon0, lon1]
+            self.lats = [lat0, lat1]
+
+            if find == True:
+                self._find_indexes(lon0, lat0,
+                                lon1, lat1,
+                                forced_limits=forced_limits)
+        elif lons.any() and lats.any():
+            print('Searching the nearest indexes for each location provided ...')
+            self.lons, self.lats = lons, lats
+            
+            # build transect from multiples profiles' locations
+            if find == True:
+                self._find_indexes_composite(lons, lats)
+
+        # second step is to extract the vertical profiles of each location found
+        if type(varbs) == str:
+            print(f'extracting transect for {varbs}')
+            # just extract a DataArray
+            self._extract_profiles(varb=varbs)
+            
+        elif type(varbs) == list:
+            for k,varb in enumerate(varbs):
+                print(f'extracting transect for {varb}')
+                # extract section
+                self._extract_profiles(varb=varb)
+                
+                if k == 0:
+                    # if it is the first variable, then we must convert a DataArray 
+                    # to Dataset, and then save the other variables
+                    self.ds = self.ds_section.to_dataset()
+                else:
+                    # save the transect into an already existent Dataset
+                    self.ds[varb] = self.ds_section
+        else:
+            raise ValueError(f"Type of varbs not available: {type(varbs)}")
+        
+        # TODO: implement temporal search
+        # third, and optional, extract profiles temporally closest to a list of datetimes
+        # self._get_temporal_profiles(times) --- UNDER CONSTRUCTION
+
+    # find transect based on a list of segments, ideal to reconstruct cross sections from
+    # CTD profiles' locations
+    def _find_indexes_composite(self, lons, lats):
+        plt.ioff()
+        
+        isec,jsec,xsec,ysec = create_section_composite(self.lon, self.lat,
+                                                        lons, lats)
+        # save the locations found
+        self.ds_locs = xr.Dataset()
+        self.ds_locs['lon'] = xr.DataArray(data=xsec, dims=('location'))
+        self.ds_locs['lat'] = xr.DataArray(data=ysec, dims=('location'))
+        self.isec = isec
+        self.jsec = jsec
+
+        # create the begin and the end of the transect in latlon pairs
+        self.begin_transect = (ysec[0], xsec[0])
+        self.final_transect = (ysec[-1], xsec[-1])
+
+    # find transect based on a begin and final location. It cross a straight line and find
+    # the nearests cells to it
+    def _find_indexes(self, lon0, lat0, lon1, lat1, forced_limits=False):
+        """
+            Given an inital and final geographical location, creates a best fit linear
+            transect and found all values near this transect in a numerical grid domain.
+            This function uses the sectionate code, developed by Raphael Dussin, that can
+            be found at:
+            https://github.com/raphaeldussin/sectionate/blob/master/sectionate/section.py
+        """
+        plt.ioff() # in case plt.ion() is active
+
+        isec,jsec,xsec,ysec = create_section(self.lon, self.lat,
+                                             lon0, lat0,
+                                             lon1, lat1)
+        # save the locations found
+        self.ds_locs = xr.Dataset()
+        self.ds_locs['lon'] = xr.DataArray(data=xsec, dims=('location'))
+        self.ds_locs['lat'] = xr.DataArray(data=ysec, dims=('location'))
+        self.isec = isec
+        self.jsec = jsec
+
+        if forced_limits:
+            # if activated, then we force the limits of the points found
+            self.ds_locs = self.ds_locs.where((self.ds_locs['lon'] >= min([lon0, lon1]))
+                                             & (self.ds_locs['lon'] <= max([lon0, lon1]))
+                                             & (self.ds_locs['lat'] <= max([lat0, lat1]))
+                                             & (self.ds_locs['lat'] <= max([lat0, lat1])),
+                                             drop=True)
+
+        # create the begin and the end of the transect in latlon pairs
+        self.begin_transect = (ysec[0], xsec[0])
+        self.final_transect = (ysec[-1], xsec[-1])
+
+    # extract vertical profiles for a given variable (varb)
+    def _extract_profiles(self, varb):
+        """
+        """
+        
+        # get list of locations
+        sec_lons, sec_lats = self.ds_locs['lon'].values, self.ds_locs['lat'].values
+        
+        # TODO: optimize processing. Taking too long 'cause of this for loop
+        # extract each profile
+        list_ds_section = []
+        for lon0,lat0 in zip(sec_lons, sec_lats):
+            profile = self.data[varb].xroms.sel2d(lon0, lat0)
+            list_ds_section.append(profile)
+            
+        self.ds_section = xr.concat(list_ds_section, dim='profiles')
+
+    # TODO: cada tempo deve estar associado à uma estação, então a solução não é trivial
+    # find the closest model profile to a given list of datetimes, to recreate the conditions
+    # which the observed transect was interpolated
+    def _get_temporal_profiles(self, times):
+        """
+        """
+        if hasattr(self, 'ds_section'):
+            list_profiles = []
+            for t in times:
+                profile = self.ds_section.sel(ocean_time=t, method='nearest')
+                list_profiles.append(profile)
+            
+            # build final transect
+            self.transect = xr.concat(list_profiles, dim='profiles')
+    
+    # simple data visualization 
+    def _plot(self, varb=None, T=-1, x='latitude', y='vertical', figsize=(10,6), cmap='magma', invertx=True):
+        """
+        """
+        plt.ion()
+        
+        self.fig = plt.figure(figsize=figsize)
+
+        self.ax_cross = self.fig.add_subplot(1,2,1)
+        self.ax_trans = self.fig.add_subplot(1,2,2, projection=ccrs.PlateCarree())
+        
+        # plot transect
+        if varb:
+            self.section = self.ds[varb].cf.isel(T=T)
+        else:
+            self.section = self.ds_section.cf.isel(T=T)
+            
+        self.section.where(~self.section.isnull(), drop=True).cf.plot(ax=self.ax_cross, x=x, y=y, cmap=cmap)
+        
+        # matplotlib plots the vertical section creating the x axis from low to high. Sometimes, we need
+        # the opposite. For that, activate invertx
+        if invertx == True:
+            self.ax_cross.invert_xaxis()
+        
+        # plot the transect location
+        self.ax_trans.coastlines('10m')
+
+        self.ax_trans.plot(self.lon, self.lat, 'k', alpha=.1)
+        self.ax_trans.plot(self.lon.T, self.lat.T, 'k', alpha=.1)
+
+        self.ax_trans.set_xlim(np.nanmin(self.lons)-.5, np.nanmax(self.lons)+.5)
+        self.ax_trans.set_ylim(np.nanmin(self.lats)-.5, np.nanmax(self.lats)+.5)
+
+        self.ax_trans.scatter(self.ds_locs['lon'], self.ds_locs['lat'], s=20,
+                   marker='o', c='red', label='Virtual profiles')
+        self.ax_trans.scatter(self.lons, self.lats, s=20, marker='s', c='green', label='Real profiles', zorder=10)
+
+        self.ax_trans.legend()
+        plt.tight_layout()
+        plt.show()
